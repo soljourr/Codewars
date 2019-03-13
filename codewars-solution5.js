4afc9aab33f467e9adae52d026bbaf1168de3936
Ricardo Soljour <Ricky@Ricardos-MacBook-Pro.local>
2019-03-13 19:58:31 -0400

    First Commit

---
+++ codewars-solution5.js
@@ -0,0 +1,19 @@
+
+//Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+//Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+let countBits = function(n) {
+  //change the value of n into base 2 as a string
+  let x= n.toString(2)
+  let counter= 0;
+  //loop through base 2 number to count every digit
+  for (i =0; i < x.length; i++){
+
+    if (x[i] === "1"){
+      counter++
+    }
+  }
+  //return the number of bits
+  return counter;
+};
